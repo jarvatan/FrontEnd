9700a1c9c8065b73a3598666af05331cc5ef85d5
jarva <tamjarva@gmail.com>
2019-02-19 14:23:43 +0800

    fix: 修改配置。

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to visit My Website"/>
+    <HelloWorld msg="Welcome to Visit My Website"/>
   </div>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -5,20 +5,8 @@ module.exports = {
   // pages:{ type:Object,Default:undfind }
   devServer: {
     port: 8085, // 端口号
-    host: 'localhost',
     disableHostCheck: true,
     https: false, // https:{type:Boolean}
-    open: true, // 配置自动启动浏览器
-    // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
-    proxy: {
-      '/api': {
-        target: '<url>',
-        ws: true,
-        changeOrigin: true
-      },
-      '/foo': {
-        target: '<other_url>'
-      }
-    } // 配置多个代理
+    open: true // 配置自动启动浏览器
   }
 }
